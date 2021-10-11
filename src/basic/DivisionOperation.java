c8f729f83f0a6fc8fb0acab1307bdb984333bcd8
Ramesh A <catchrameshhere@gmai.com>
2021-10-12 00:23:16 +0530

    Added the division operation file

---
+++ src/basic/DivisionOperation.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class DivisionOperation {
+	
+	public void add(int a, int b) {
+		System.out.println("Result of division operation = " + (a/b));
+	}
+	
+
+	public static void main(String[] args) {
+		DivisionOperation obj = new DivisionOperation();
+		obj.add(50, 5);
+
+	}
+
+}
